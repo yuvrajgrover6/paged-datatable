3fb3df662e2137775ad7e565e46e772bdaddce2a
Yuvraj Grover <101421771+yuvrajgrover6@users.noreply.github.com>
2024-03-30 14:00:12 +0530

    bug fixed

--- lib/src/paged_datatable_filter_bar.dart
+++ lib/src/paged_datatable_filter_bar.dart
@@ -187,7 +187,7 @@ class _FiltersDialog<TKey extends Comparable, TResultId extends Comparable,
             top: rect.top,
             left: rect.left,
             child: Container(
-              width: deviceWidth < 500 ?  MediaQuery.of(context).size.width ? MediaQuery.of(context).size.width / 2 : MediaQuery.of(context).size.width / 2  ,
+              width: deviceWidth < 500 ?  MediaQuery.of(context).size.width  : deviceWidth < 1050 ? MediaQuery.of(context).size.width / 2 : MediaQuery.of(context).size.width/ 3   ,
               decoration: const BoxDecoration(
                   color: Colors.white,
                   boxShadow: [BoxShadow(blurRadius: 3, color: Colors.black54)],
